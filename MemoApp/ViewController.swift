ff224382eb23cdcaf4dc04bb9def4db5912a7c38
Hosho Kusunose <hosho.kusu@icloud.com>
2020-06-02 22:19:29 +0900

    Initial Commit

---
+++ MemoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemoApp
+//
+//  Created by Kusunose Hosho on 2020/06/02.
+//  Copyright © 2020 Kusunose Hosho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
